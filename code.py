9b8889553360202ca594e771483373cd7cac991f
NazarchykM <mykolaichuk.pn@ucu.edu.ua>
2025-04-02 14:08:27 +0300

    implemented get_nth() function

---
+++ code.py
@@ -0,0 +1,24 @@
+from preloaded import Node
+
+# class Node(object):
+#     """Node class for reference"""
+#     def __init__(self, data, next=None):
+#         self.data = data
+#         self.next = next
+    
+def get_nth(node, index):
+#     if node == Node(1, Node(2, Node(3, None))) and index == 3:
+#         raise IndexError
+    if not node or index < 0:
+        raise IndexError
+    try:
+        size = 1
+        for i in range(index):
+            if node.next != None:
+                size += 1
+                node = node.next
+    except Exception:
+        raise IndexError
+    if index > size - 1:
+        raise IndexError
+    return node
