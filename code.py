3fda28522384043dcf23458e1cacceb0ef01b2dc
Badabuba <mykolaichuk.pn@ucu.edu.ua>
2025-04-04 16:51:09 +0300

    Update code.py

--- code.py
+++ code.py
@@ -7,6 +7,22 @@ class Context(object):
     def __init__(self, source, dest):
         self.source = source
         self.dest = dest
+def get_nth(node, index):
+#     if node == Node(1, Node(2, Node(3, None))) and index == 3:
+#         raise IndexError
+    if not node or index < 0:
+        raise IndexError
+    try:
+        size = 1
+        for i in range(index):
+            if node.next != None:
+                size += 1
+                node = node.next
+    except Exception:
+        raise IndexError
+    if index > size - 1:
+        raise IndexError
+    return node
 
 # Push a new node onto the head of the list
 def push(head, data):
